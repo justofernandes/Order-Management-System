8fac9c93070106e951326b185d8c06dc5cfaa83c
marcostommaz <marcos.tomaz.103@ufrn.edu.br>
2025-05-22 20:14:47 -0300

    filas implementadas

--- fila.c
+++ fila.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "fila.h"
+
+void inicializarFila(Fila *f) {
+    f->inicio = NULL;
+    f->fim = NULL;
+}
+
+void enfileirar(Fila *f, Pedido *p) {
+    p->prox = NULL;
+    if (f->fim == NULL) {
+        f->inicio = f->fim = p;
+    } else {
+        f->fim->prox = p;
+        f->fim = p;
+    }
+}
+
+Pedido* desenfileirar(Fila *f) {
+    if (f->inicio == NULL) return NULL;
+    Pedido *removido = f->inicio;
+    f->inicio = f->inicio->prox;
+    if (f->inicio == NULL)
+        f->fim = NULL;
+    return removido;
+}
+
+
+int filaVazia(Fila *f) {
+    return f->inicio == NULL;
+}
+
+
+
+//falta implementarmos alguma coisa para listarmos os pedidos, temos que pensar num identificador que voce vai colocar ai na lista
--- fila.h
+++ fila.h
@@ -1,9 +1,26 @@
 #ifndef FILA_H
 #define FILA_H
 
+typedef struct {
+    Pedido *inicio;
+    Pedido *fim;
+} Fila;
 
+typedef struct Prato {
+    char nome[100];
+    struct Prato *prox;
+} Prato;
 
+typedef struct Pedido {
+    
+    Prato *pratos;
+    struct Pedido *prox;
+} Pedido;
 
+void inicializarFila(Fila *f);
+void enfileirar(Fila *f, Pedido *p);
+Pedido* desenfileirar(Fila *f);
+int filaVazia(Fila *f);
 
 
 #endif
