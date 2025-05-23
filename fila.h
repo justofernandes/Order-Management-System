4632b59f89d33ad6bb7087a8354f8a7cb314bac3
marcostommaz <marcos.tomaz.103@ufrn.edu.br>
2025-05-23 12:04:54 -0300

    atualizacao na fila

--- fila.h
+++ fila.h
@@ -1,26 +1,23 @@
 #ifndef FILA_H
 #define FILA_H
 
+typedef struct Pedido {
+    int num_pedido;
+    char entrada[60];
+    char principal[60];
+    char sobremesa [60];
+    struct Pedido *prox;
+} Pedido;
+
 typedef struct {
     Pedido *inicio;
     Pedido *fim;
 } Fila;
 
-typedef struct Prato {
-    char nome[100];
-    struct Prato *prox;
-} Prato;
-
-typedef struct Pedido {
-    
-    Prato *pratos;
-    struct Pedido *prox;
-} Pedido;
-
 void inicializarFila(Fila *f);
 void enfileirar(Fila *f, Pedido *p);
 Pedido* desenfileirar(Fila *f);
 int filaVazia(Fila *f);
-
+void listarFila(Fila f);
 
 #endif
